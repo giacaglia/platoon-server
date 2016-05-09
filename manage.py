76ac66f31d35c4480224e2ee17c0bca5c712c372
Giuliano Pezzolo Giacaglia <raulzito234@gmail.com>
2016-05-09 14:33:50 -0400

    Platoon server

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
-#!/usr/bin/env python
 import os
-import sys
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+from app import hello, db
 
-    from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
