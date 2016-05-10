0026b08db66d207fd0bbde4cc5dc94d789c20301
Giuliano Giacaglia <raulzito234@gmail.com>
2016-05-10 19:00:48 -0400

    Add template

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -10,10 +10,21 @@ db = SQLAlchemy(app)
 
 from models import Result
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, results=results)
 
 
 if __name__ == '__main__':
