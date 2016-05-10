43b58cf090b879e9edf877b2da4d0cb2a4bccab7
Giuliano Giacaglia <raulzito234@gmail.com>
2016-05-10 19:18:54 -0400

    Add count of words

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-mport os
+import os
 import requests
 import operator
 import re
@@ -50,7 +50,7 @@ def index():
                 no_stop_words_count.items(),
                 key=operator.itemgetter(1),
                 reverse=True
-            )
+            )[:10]
             try:
                 result = Result(
                     url=url,
