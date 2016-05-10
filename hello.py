e8798a33fb5dc083ab48ea34aa412c5096467e73
Giuliano Giacaglia <raulzito234@gmail.com>
2016-05-10 18:43:40 -0400

    Form to submimit url

--- hello.py
+++ hello.py
@@ -1,17 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-print(os.environ['APP_SETTINGS'])
+from models import Result
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
