69484068f4fc6588d3ded4a8f35b87657b938aa3
Giuliano Giacaglia <raulzito234@gmail.com>
2016-05-11 11:16:10 -0400

    New postgress db and redis job list

--- README.md
+++ README.md
@@ -25,8 +25,8 @@ $ createdb python_getting_started
 $ python manage.py migrate
 $ python manage.py collectstatic
 
-$ heroku local
-```
+$ brew install redis
+$ heroku local`
 
 Your app should now be running on [localhost:5000](http://localhost:5000/).
 
--- hello.py
+++ hello.py
@@ -8,15 +8,18 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
-
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-from models import Result
+q = Queue(connection=conn)
 
+from models import *
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
